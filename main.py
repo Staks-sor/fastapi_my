44168770ad90a4cca4aadea659c70546581f5291
staks <stas.sor@gmail.com>
2024-10-04 14:58:53 +0300

    мой комит

---
+++ main.py
@@ -0,0 +1,78 @@
+import uvicorn
+from fastapi import FastAPI, Query, Body
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Dubai", "name": "dubai"},
+]
+
+
+@app.put("/hotels/{hotel_id}")
+def put_change_all():
+    ...
+
+
+@app.patch("/hotels/{hotel_id}")
+def patch_change_uniq():
+    ...
+
+
+@app.post("/hotels")
+def create_hotel(
+        title: str = Body(embed=True),
+):
+    global hotels
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "ok"}
+
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotels(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+
+    return {"status": "ok"}
+
+
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description="Айди отеля"),
+        title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app")

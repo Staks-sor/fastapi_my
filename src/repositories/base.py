25083f338a9e961c019d91bc75237af07fa47617
staks <stas.sor@gmail.com>
2024-10-21 18:42:55 +0300

    убрал лишнее из импортов

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -2,8 +2,6 @@ from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import select, insert, update, delete
 
-from src.schemas.hotels import Hotel
-
 
 class BaseRepository:
     model = None
@@ -28,7 +26,8 @@ class BaseRepository:
     async def add(self, data: BaseModel):
         add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         result = await self.session.execute(add_data_stmt)
-        return result.scalars().one()
+        model = result.scalars().one()
+        return self.schema.model_validate(model)
 
     async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by) -> None:
         update_stmt = (

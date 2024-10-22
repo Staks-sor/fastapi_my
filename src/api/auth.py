b5d2c795a27c86d9bf6f92e3aff5c2407e58fb77
staks <stas.sor@gmail.com>
2024-10-22 06:49:00 +0300

    Добавил авторизацию. Увы пора идти на работу. :( лучше писать...

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from datetime import timedelta, datetime, timezone
 
+from fastapi import APIRouter, HTTPException
+import jwt
 from passlib.context import CryptContext
 
 from src.database import async_session_maker
@@ -9,6 +11,17 @@ from src.schemas.users import UserRequestAdd, UserAdd
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post("/register")
@@ -22,3 +35,17 @@ async def register_user(
         await session.commit()
 
     return {"status": "ok"}
+
+
+@router.post("/login")
+async def register_user(
+        data: UserRequestAdd,
+):
+    # hashed_password = pwd_context.hash(data.password)
+    # new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Авторизация не выполнена")
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}

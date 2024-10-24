530b482cc3a5adf561ec305cabe634bbc6644b26
staks <stas.sor@gmail.com>
2024-10-24 06:10:44 +0300

    небольшой рефакторинг: Перенес регистрацию в сервис. в поинтах уже использую класс из сервиса.

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,36 +1,20 @@
-from datetime import timedelta, datetime, timezone
 
 from fastapi import APIRouter, HTTPException, Response
-import jwt
-from passlib.context import CryptContext
 
-from src.config import settings
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= {"exp": expire}
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
-    return encoded_jwt
 
 
 @router.post("/register")
 async def register_user(
         data: UserRequestAdd,
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
@@ -48,7 +32,7 @@ async def login_user(
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(status_code=401, detail="Авторизация не выполнена")
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль не верный")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
         return {"access_token": access_token}
---
+++ src/services/auth.py
@@ -0,0 +1,22 @@
+from datetime import timedelta, datetime, timezone
+import jwt
+from passlib.context import CryptContext
+
+from src.config import settings
+
+
+class AuthService:
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def create_access_token(self, data: dict) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode |= {"exp": expire}
+        encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
+        return encoded_jwt
+
+    def hash_password(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)

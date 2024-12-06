5a3a87694f7c65826ace89ad073ffa7067db52c8
staks <stas.sor@gmail.com>
2024-12-06 14:42:03 +0300

    заменил схемы на мапперс в юзерс репозиторий

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -3,12 +3,13 @@ from sqlalchemy import select
 
 from src.models.users import UsersOrm
 from src.repositories.base import BaseRepository
+from src.repositories.mappers.mappers import UserDataMapper
 from src.schemas.users import User, UserWithHashedPassword
 
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
-    schema = User
+    mapper = UserDataMapper
 
     async def get_user_with_hashed_password(self, email: EmailStr):
         query = select(self.model).filter_by(email=email)

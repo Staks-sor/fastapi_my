52e891050c48a13eed06fc36b253049b8630ee22
staks <stas.sor@gmail.com>
2024-12-06 14:42:20 +0300

    заменил схемы на мапперс в комтах репозиторий

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -5,15 +5,15 @@ from sqlalchemy.orm import joinedload, selectinload
 
 from src.models.rooms import RoomsORM  # Модель, представляющая таблицу с комнатами (rooms)
 from src.repositories.base import BaseRepository  # Базовый репозиторий, обеспечивающий базовые операции с БД
+from src.repositories.mappers.mappers import RoomDataMapper, RoomDataWithRelsMapper
 from src.repositories.utils import rooms_ids_for_booking
-from src.schemas.rooms import Room, RoomWithRels  # Pydantic-схема для комнат
 
 
 # Репозиторий для работы с комнатами (rooms)
 class RoomsRepository(BaseRepository):
     # Связываем ORM-модель и Pydantic-схему с репозиторием
     model = RoomsORM  # Модель базы данных для таблицы rooms
-    schema = Room  # Схема данных для сериализации комнат
+    mapper = RoomDataMapper  # Схема данных для сериализации комнат
 
     # Асинхронный метод для получения свободных комнат в заданный период времени
     async def get_filtered_by_time(
@@ -23,7 +23,7 @@ class RoomsRepository(BaseRepository):
             date_to: date,  # Дата окончания периода
     ):
         # Подзапрос для подсчета количества занятых комнат по ID комнаты
-        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id,)
+        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id, )
 
         query = (
             select(self.model)
@@ -31,7 +31,7 @@ class RoomsRepository(BaseRepository):
             .filter(RoomsORM.id.in_(rooms_ids_to_get))
         )
         result = await self.session.execute(query)
-        return [RoomWithRels.model_validate(model) for model in result.unique().scalars().all()]
+        return [RoomDataWithRelsMapper.map_to_domain_entity(model) for model in result.unique().scalars().all()]
 
     async def get_one_or_none_with_rels(self, **filter_by):
         query = (
@@ -43,4 +43,4 @@ class RoomsRepository(BaseRepository):
         model = result.scalars().one_or_none()
         if model is None:
             return None
-        return RoomWithRels.model_validate(model)
+        return RoomDataWithRelsMapper.map_to_domain_entity(model)
